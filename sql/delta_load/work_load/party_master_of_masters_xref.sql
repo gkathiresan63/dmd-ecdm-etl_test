4649a03e4ee5a44c8dc891bb13aa6493bc2d47bf
gkathiresan63 <65361027+gkathiresan63@users.noreply.github.com>
2020-06-03 15:48:28 -0400

    party-master_of_masters - dml

---
+++ sql/delta_load/work_load/party_master_of_masters_xref.sql
@@ -0,0 +1,296 @@
+TRUNCATE TABLE EDW_WORK.PARTY_MASTER_OF_MASTERS_XREF;
+TRUNCATE TABLE EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK;
+
+-- Populate the Pre work table 
+--  Apply dedup logic
+
+INSERT INTO EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    PARTY_ID,                             
+    PARTY_PRIOR_ID,                      
+    SOR_PARTY_ID,                        
+    PARTY_ID_TYPE_CDE,                  
+    BEGIN_DT,                         
+    BEGIN_DTM,                         
+    ROW_PROCESS_DTM,                     
+    AUDIT_ID,                          
+    LOGICAL_DELETE_IND,                   
+    CHECK_SUM,                      
+    CURRENT_ROW_IND,                    
+    END_DT,                           
+    END_DTM,                           
+    SOURCE_SYSTEM_ID,                  
+    RESTRICTED_ROW_IND,                
+    PARTY_ID_STATUS_TYPE_CDE,           
+    PARTY_ID_CLASS_TYPE_CDE,
+    ADD_UPD_DEL_IND          
+)
+SELECT 
+    UUID_GEN(PREHASH_VALUE(CLEAN_STRING(SRC.MEMBER_ID)))::UUID AS DIM_PARTY_NATURAL_KEY_HASH_UUID,
+    NULL                                                       AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    CLEAN_STRING(SRC.MEMBER_ID)                                AS PARTY_ID,
+    NULL                                                       AS PARTY_PRIOR_ID,
+    CLEAN_STRING(SRC.ALT_ID_VAL)                               AS SOR_PARTY_ID,
+    CLEAN_STRING(SRC.ALT_ID_TP)                                AS PARTY_ID_TYPE_CDE,
+    SRC.SRC_LST_MOD_TS::DATE                                   AS BEGIN_DT,
+    SRC.SRC_LST_MOD_TS::TIMESTAMP                              AS BEGIN_DTM,
+    CURRENT_TIMESTAMP(6)                                       AS ROW_PROCESS_DTM,
+    :audit_id                                                  AS AUDIT_ID,
+    FALSE                                                      AS LOGICAL_DELETE_IND,
+    UUID_GEN(PREHASH_VALUE(
+        CLEAN_STRING(SRC.ALT_ID_VAL),
+        CLEAN_STRING(SRC.ALT_ID_TP),
+        CLEAN_STRING(SRC.STS_CD),
+        CLEAN_STRING(SRC.ALT_ID_STYP_CD)
+        ))::UUID                                                AS CHECK_SUM,
+    TRUE                                                        AS CURRENT_ROW_IND,
+    '9999-12-31'                                                AS END_DT,
+    '9999-12-31'::TIMESTAMP                                     AS END_DTM,
+    '45'                                                        AS SOURCE_SYSTEM_ID,
+    FALSE                                                       AS RESTRICTED_ROW_IND,
+    CLEAN_STRING(SRC.STS_CD) AS PARTY_ID_STATUS_TYPE_CDE,
+    CLEAN_STRING(SRC.ALT_ID_STYP_CD) AS PARTY_ID_STATUS_TYPE_CDE,
+    SRC.ADD_UPD_DEL_IND                                         AS ADD_UPD_DEL_IND
+FROM
+(
+SELECT
+    SRC_DUP.*,
+    ROW_NUMBER() OVER (PARTITION BY CLEAN_STRING(SRC_DUP.MEMBER_ID) ORDER BY SRC_DUP.SRC_LST_MOD_TS DESC) AS RNK
+
+FROM 
+   EDW_STAGING.ECDM_C_B_PARTY_ALT_ID_OUT SRC_DUP
+)SRC
+WHERE SRC.RNK = 1;
+
+
+-- Apply delete records
+INSERT INTO EDW_WORK.PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    PARTY_ID,                             
+    PARTY_PRIOR_ID,                      
+    SOR_PARTY_ID,                        
+    PARTY_ID_TYPE_CDE,                  
+    BEGIN_DT,                         
+    BEGIN_DTM,                         
+    ROW_PROCESS_DTM,                     
+    AUDIT_ID,                          
+    LOGICAL_DELETE_IND,                   
+    CHECK_SUM,                      
+    CURRENT_ROW_IND,                    
+    END_DT,                           
+    END_DTM,                           
+    SOURCE_SYSTEM_ID,                  
+    RESTRICTED_ROW_IND,                
+    PARTY_ID_STATUS_TYPE_CDE,           
+    PARTY_ID_CLASS_TYPE_CDE           
+)
+SELECT
+    SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID         AS DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    SRC.DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID   AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    SRC.PARTY_ID                                AS PARTY_ID,                             
+    SRC.PARTY_PRIOR_ID                          AS PARTY_PRIOR_ID,                      
+    SRC.SOR_PARTY_ID                            AS SOR_PARTY_ID,                        
+    SRC.PARTY_ID_TYPE_CDE                       AS PARTY_ID_TYPE_CDE,                  
+    --CASE WHEN TGT.BEGIN_DT IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DT END AS BEGIN_DT,
+    --CASE WHEN TGT.BEGIN_DTM IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DTM END AS BEGIN_DTM,
+    CASE WHEN TGT.BEGIN_DT IS NULL THEN  SRC.BEGIN_DT  ELSE TGT.BEGIN_DT END AS BEGIN_DT,
+    CASE WHEN TGT.BEGIN_DTM IS NULL THEN SRC.BEGIN_DTM ELSE TGT.BEGIN_DTM END AS BEGIN_DTM,
+    SRC.ROW_PROCESS_DTM                         AS ROW_PROCESS_DTM,                     
+    SRC.AUDIT_ID                                AS AUDIT_ID,                          
+    TRUE                                        AS LOGICAL_DELETE_IND,                   
+    SRC.CHECK_SUM                               AS CHECK_SUM,                      
+    FALSE                                       AS CURRENT_ROW_IND,                    
+    SRC.END_DT                                  AS END_DT,                           
+    SRC.END_DTM                                 AS END_DTM,                           
+    SRC.SOURCE_SYSTEM_ID                        AS SOURCE_SYSTEM_ID,                  
+    SRC.RESTRICTED_ROW_IND                      AS RESTRICTED_ROW_IND,                
+    SRC.PARTY_ID_STATUS_TYPE_CDE                AS PARTY_ID_STATUS_TYPE_CDE,           
+    SRC.PARTY_ID_CLASS_TYPE_CDE                 AS PARTY_ID_CLASS_TYPE_CDE     
+
+FROM
+--  Apply Delete logic . If record present use tgt begin date ele src begin date
+EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK SRC
+LEFT JOIN EDW_TDSUNSET.PARTY_MASTER_OF_MASTERS_XREF TGT
+ON SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID::VARCHAR = TGT.DIM_PARTY_NATURAL_KEY_HASH_UUID::VARCHAR 
+AND TGT.CURRENT_ROW_IND = TRUE
+WHERE SRC.ADD_UPD_DEL_IND = 'D';
+-- AND TGT.DIM_PARTY_NATURAL_KEY_HASH_UUID IS NOT NULL  -- check whether this is needed
+
+
+
+-- Apply insert part for all records
+INSERT INTO EDW_WORK.PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    PARTY_ID,                             
+    PARTY_PRIOR_ID,                      
+    SOR_PARTY_ID,                        
+    PARTY_ID_TYPE_CDE,                  
+    BEGIN_DT,                         
+    BEGIN_DTM,                         
+    ROW_PROCESS_DTM,                     
+    AUDIT_ID,                          
+    LOGICAL_DELETE_IND,                   
+    CHECK_SUM,                      
+    CURRENT_ROW_IND,                    
+    END_DT,                           
+    END_DTM,                           
+    SOURCE_SYSTEM_ID,                  
+    RESTRICTED_ROW_IND,                
+    PARTY_ID_STATUS_TYPE_CDE,           
+    PARTY_ID_CLASS_TYPE_CDE           
+)
+SELECT
+    SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID         AS DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    SRC.DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID   AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    SRC.PARTY_ID                                AS PARTY_ID,                             
+    SRC.PARTY_PRIOR_ID                          AS PARTY_PRIOR_ID,                      
+    SRC.SOR_PARTY_ID                            AS SOR_PARTY_ID,                        
+    SRC.PARTY_ID_TYPE_CDE                       AS PARTY_ID_TYPE_CDE,                  
+    --CASE WHEN TGT.BEGIN_DT IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DT END AS BEGIN_DT,
+    --CASE WHEN TGT.BEGIN_DTM IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DTM END AS BEGIN_DTM,
+    '0001-01-01'                                AS BEGIN_DT,
+    '0001-01-01 00:00:00'                       AS BEGIN_DTM,
+    SRC.ROW_PROCESS_DTM                         AS ROW_PROCESS_DTM,                     
+    SRC.AUDIT_ID                                AS AUDIT_ID,                          
+    FALSE                                       AS LOGICAL_DELETE_IND,                   
+    SRC.CHECK_SUM                               AS CHECK_SUM,                      
+    TRUE                                        AS CURRENT_ROW_IND,                    
+    SRC.END_DT                                  AS END_DT,
+    SRC.END_DTM                                 AS END_DTM,                        
+    SRC.SOURCE_SYSTEM_ID                        AS SOURCE_SYSTEM_ID,                  
+    SRC.RESTRICTED_ROW_IND                      AS RESTRICTED_ROW_IND,                
+    SRC.PARTY_ID_STATUS_TYPE_CDE                AS PARTY_ID_STATUS_TYPE_CDE,           
+    SRC.PARTY_ID_CLASS_TYPE_CDE                 AS PARTY_ID_CLASS_TYPE_CDE     
+
+FROM
+--  Apply INSERT logic . If begin date AS 0001-01-01
+EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK SRC
+WHERE
+    SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID
+        NOT IN (SELECT DISTINCT DIM_PARTY_NATURAL_KEY_HASH_UUID FROM EDW_TDSUNSET.PARTY_MASTER_OF_MASTERS_XREF WHERE CURRENT_ROW_IND = TRUE)
+AND SRC.ADD_UPD_DEL_IND = 'I';
+
+
+
+
+-- For update we need to have two records, one for prev another for curr
+-- Curr
+
+---------------- check the below curr update is needed or can be take can with insert/update --------
+INSERT INTO EDW_WORK.PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    PARTY_ID,                             
+    PARTY_PRIOR_ID,                      
+    SOR_PARTY_ID,                        
+    PARTY_ID_TYPE_CDE,                  
+    BEGIN_DT,                         
+    BEGIN_DTM,                         
+    ROW_PROCESS_DTM,                     
+    AUDIT_ID,                          
+    LOGICAL_DELETE_IND,                   
+    CHECK_SUM,                      
+    CURRENT_ROW_IND,                    
+    END_DT,                           
+    END_DTM,                           
+    SOURCE_SYSTEM_ID,                  
+    RESTRICTED_ROW_IND,                
+    PARTY_ID_STATUS_TYPE_CDE,           
+    PARTY_ID_CLASS_TYPE_CDE           
+)
+SELECT
+    SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID         AS DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    SRC.DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID   AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    SRC.PARTY_ID                                AS PARTY_ID,                             
+    SRC.PARTY_PRIOR_ID                          AS PARTY_PRIOR_ID,                      
+    SRC.SOR_PARTY_ID                            AS SOR_PARTY_ID,                        
+    SRC.PARTY_ID_TYPE_CDE                       AS PARTY_ID_TYPE_CDE,                  
+    --CASE WHEN TGT.BEGIN_DT IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DT END AS BEGIN_DT,
+    --CASE WHEN TGT.BEGIN_DTM IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DTM END AS BEGIN_DTM,
+    SRC.BEGIN_DT                                AS BEGIN_DT,
+    SRC.BEGIN_DTM                               AS BEGIN_DTM,
+    SRC.ROW_PROCESS_DTM                         AS ROW_PROCESS_DTM,                     
+    SRC.AUDIT_ID                                AS AUDIT_ID,                          
+    FALSE                                       AS LOGICAL_DELETE_IND,                   
+    SRC.CHECK_SUM                               AS CHECK_SUM,                      
+    TRUE                                        AS CURRENT_ROW_IND,                    
+    SRC.END_DT                                  AS END_DT,
+    SRC.END_DTM                                 AS END_DTM,                         
+    SRC.SOURCE_SYSTEM_ID                        AS SOURCE_SYSTEM_ID,                  
+    SRC.RESTRICTED_ROW_IND                      AS RESTRICTED_ROW_IND,                
+    SRC.PARTY_ID_STATUS_TYPE_CDE                AS PARTY_ID_STATUS_TYPE_CDE,           
+    SRC.PARTY_ID_CLASS_TYPE_CDE                 AS PARTY_ID_CLASS_TYPE_CDE     
+
+FROM
+--  Apply UPDATE logic . If begin date = SRC_DATE and end_date = 9999 for current records.
+EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK SRC
+WHERE
+    SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID
+        IN (SELECT DISTINCT DIM_PARTY_NATURAL_KEY_HASH_UUID FROM EDW_TDSUNSET.PARTY_MASTER_OF_MASTERS_XREF WHERE CURRENT_ROW_IND = TRUE)
+AND SRC.ADD_UPD_DEL_IND = 'U';
+
+-- Prev
+INSERT INTO EDW_WORK.PARTY_MASTER_OF_MASTERS_XREF
+(
+    DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    PARTY_ID,                             
+    PARTY_PRIOR_ID,                      
+    SOR_PARTY_ID,                        
+    PARTY_ID_TYPE_CDE,                  
+    BEGIN_DT,                         
+    BEGIN_DTM,                         
+    ROW_PROCESS_DTM,                     
+    AUDIT_ID,                          
+    LOGICAL_DELETE_IND,                   
+    CHECK_SUM,                      
+    CURRENT_ROW_IND,                    
+    END_DT,                           
+    END_DTM,                           
+    SOURCE_SYSTEM_ID,                  
+    RESTRICTED_ROW_IND,                
+    PARTY_ID_STATUS_TYPE_CDE,           
+    PARTY_ID_CLASS_TYPE_CDE           
+)
+SELECT
+    TGT.DIM_PARTY_NATURAL_KEY_HASH_UUID         AS DIM_PARTY_NATURAL_KEY_HASH_UUID,     
+    TGT.DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID   AS DIM_PRIOR_PARTY_NATURAL_KEY_HASH_UUID,
+    TGT.PARTY_ID                                AS PARTY_ID,                             
+    TGT.PARTY_PRIOR_ID                          AS PARTY_PRIOR_ID,                      
+    TGT.SOR_PARTY_ID                            AS SOR_PARTY_ID,                        
+    TGT.PARTY_ID_TYPE_CDE                       AS PARTY_ID_TYPE_CDE,                  
+    --CASE WHEN TGT.BEGIN_DT IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DT END AS BEGIN_DT,
+    --CASE WHEN TGT.BEGIN_DTM IS NULL THEN '0001-01-01' ELSE TGT.BEGIN_DTM END AS BEGIN_DTM,
+    TGT.BEGIN_DT                                AS BEGIN_DT,
+    TGT.BEGIN_DTM                               AS BEGIN_DTM,
+    SRC.ROW_PROCESS_DTM                         AS ROW_PROCESS_DTM,                     
+    SRC.AUDIT_ID                                AS AUDIT_ID,                          
+    FALSE                                       AS LOGICAL_DELETE_IND,                   
+    TGT.CHECK_SUM                               AS CHECK_SUM,                      
+    FALSE                                       AS CURRENT_ROW_IND,                    
+    SRC.BEGIN_DT - INTERVAL '1 day'             AS END_DT,
+    SRC.BEGIN_DTM - INTERVAL '1 second'         AS END_DTM,                          
+    TGT.SOURCE_SYSTEM_ID                        AS SOURCE_SYSTEM_ID,                  
+    TGT.RESTRICTED_ROW_IND                      AS RESTRICTED_ROW_IND,                
+    TGT.PARTY_ID_STATUS_TYPE_CDE                AS PARTY_ID_STATUS_TYPE_CDE,           
+    TGT.PARTY_ID_CLASS_TYPE_CDE                 AS PARTY_ID_CLASS_TYPE_CDE     
+
+     
+    FROM
+--  Apply UPDATE logic . If begin date = SRC_DATE and end_date = 9999 for current records.
+EDW_STAGING.PARTY_MASTER_OF_MASTERS_XREF_PRE_WORK SRC
+
+LEFT JOIN EDW_TDSUNSET.PARTY_MASTER_OF_MASTERS_XREF TGT 
+ON SRC.DIM_PARTY_NATURAL_KEY_HASH_UUID = TGT.DIM_PARTY_NATURAL_KEY_HASH_UUID
+AND TGT.CURRENT_ROW_IND = TRUE
+WHERE
+   TGT.DIM_PARTY_NATURAL_KEY_HASH_UUID IS NOT NULL
+   AND SRC.ADD_UPD_DEL_IND = 'U';
+
